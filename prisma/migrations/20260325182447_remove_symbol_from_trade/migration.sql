d102e8002d8f09d4ebb4dcfdce7337aef9b062fc
Abhishek Mishra <abhim5920@gmail.com>
2026-03-25 19:04:58 +0000

    fix: remove symbol from Trade schema

---
+++ prisma/migrations/20260325182447_remove_symbol_from_trade/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `symbol` on the `Trade` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Trade" DROP COLUMN "symbol";
