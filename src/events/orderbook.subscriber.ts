5b889fd8aca7f94ca43f07b02651306b3e8d2e21
Abhishek Mishra <abhim5920@gmail.com>
2026-03-27 09:45:05 +0000

    feat: integrate events with websocket

--- src/events/orderbook.subscriber.ts
+++ src/events/orderbook.subscriber.ts
@@ -4,7 +4,6 @@ import { eventBus } from "./eventBus"
 
 export const initOrderbookSubscriber = () => {
     eventBus.on("orderbook_update", ( {marketId, data}:{marketId:number, data: Orderbook} )=>{
-        console.log(marketId, " : ")
-        console.dir(data, {depth: null})
+        broadcastToMarket(marketId, data)
     })
 }
